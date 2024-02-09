feba83bffffe4909508579ce3c9b1dd5eb14ec66
chaudharykapil <chaudharykapil8279@gmail.com>
2024-02-09 16:14:27 +0530

    adding tailwind

--- main.js
+++ main.js
@@ -1,61 +1,29 @@
-const electronApp = require('electron').app;
-const electronBrowserWindow = require('electron').BrowserWindow;
-const electronIpcMain = require('electron').ipcMain;
-
-const nodePath = require("path");
-
-// Prevent garbage collection
-let window;
-
-function createWindow() {
-    return new electronBrowserWindow({
-        x: 0,
-        y: 0,
-        width: 800,
-        height: 600,
-        show: false,
-        webPreferences: {
-            nodeIntegration: false,
-            contextIsolation: true,
-            preload: nodePath.join(__dirname, 'preload.js')
-        }
-    });
-}
-
-function showMainWindow() {
-    window.loadFile('login.html')
-        .then(() => { window.show(); })
-}
-
-function showLoginWindow() {
-    // window.loadURL('https://www.your-site.com/login')
-    window.loadFile('index.html') // For testing purposes only
-        .then(() => { window.show(); })
-}
-
-electronApp.on('ready', () => {
-    window = createWindow();
-    showMainWindow();
-});
-
-electronApp.on('window-all-closed', () => {
+const {app,BrowserWindow} = require("electron")
+const path = require('node:path')
+function createWindow () {
+    const win = new BrowserWindow({
+      width: 800,
+      height: 600,
+      webPreferences: {
+        nodeIntegration:true
+      }            
+    })
+  
+    win.loadFile('./src/renderer/test/test.html')
+  }
+  
+  app.whenReady().then(() => {
+    createWindow()
+  
+    app.on('activate', () => {
+      if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow()
+      }
+    })
+  })
+  
+  app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') {
-        electronApp.quit();
+      app.quit()
     }
-});
-
-electronApp.on('activate', () => {
-    if (electronBrowserWindow.getAllWindows().length === 0) {
-        createWindow();
-    }
-});
-
-// ----- IPC -----
-
-electronIpcMain.on('message:loginShow', (event) => {
-    showLoginWindow();
-})
-
-electronIpcMain.on('message:loginSuccessful', (event, session) => {
-    showMainWindow();
-})
+  })
--- renderer.js
+++
@@ -1,9 +0,0 @@
-// document.addEventListener('DOMContentLoaded', () => {
-//   const btn = document.getElementById('btnEd');
-
-//   console.log(btn); // HTMLButtonElement object
-
-//   btn.addEventListener('click', () => {
-//     window.open('index.html', null, "width=1200,height=714,resizable=1");
-//   });
-// });
