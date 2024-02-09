860455d0a571ede560fb0ae4591b59c2b2d789c9
chaudharykapil <chaudharykapil8279@gmail.com>
2024-02-09 23:22:22 +0530

    reinit

--- main.js
+++ main.js
@@ -1,29 +1,33 @@
-const {app,BrowserWindow} = require("electron")
-const path = require('node:path')
-function createWindow () {
-    const win = new BrowserWindow({
-      width: 800,
-      height: 600,
-      webPreferences: {
-        nodeIntegration:true
-      }            
-    })
-  
-    win.loadFile('./src/renderer/test/test.html')
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+
+  const startURL = 'http://localhost:3000'
+  mainWindow.loadURL(startURL);
+
+  mainWindow.on('closed', () => (mainWindow = null));
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
   }
-  
-  app.whenReady().then(() => {
-    createWindow()
-  
-    app.on('activate', () => {
-      if (BrowserWindow.getAllWindows().length === 0) {
-        createWindow()
-      }
-    })
-  })
-  
-  app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') {
-      app.quit()
-    }
-  })
+});
+
+app.on('activate', () => {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
